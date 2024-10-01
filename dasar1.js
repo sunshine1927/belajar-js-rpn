4e9937363e81e0f471626ec59b53f4de0f99f9cf
harry febiansyah <harryfebiansyah13@gmail.com>
2024-10-02 06:44:12 +0800

    upload pembelajaran js

---
+++ dasar1.js
@@ -0,0 +1,33 @@
+// cara membuat variabel
+let b = 'b';
+const c = 'c'
+
+// variabel bisa menampung tipe data string (text), integer/number, boolean, array, object
+
+let nama = "harry"; //string
+let umur = "24"; //number
+let apakahBenar = true; //boolean
+let uang = 5000.5 //number tapi ada koma
+
+// operator aritmatika
+
+let angka1 = 20;
+let angka2 = 30;
+
+console.log(angka1 + angka2);
+console.log(angka1 - angka2);
+console.log(angka1 / angka2);
+console.log(angka1 * angka2);
+console.log(angka1 % angka2);
+
+let penampungHasilOperatorAritmatika = angka1 + angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+penampungHasilOperatorAritmatika = angka1 - angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+penampungHasilOperatorAritmatika = angka1 / angka2;
+console.log(penampungHasilOperatorAritmatika);
+
+penampungHasilOperatorAritmatika = angka1 * angka2;
+console.log(penampungHasilOperatorAritmatika);
